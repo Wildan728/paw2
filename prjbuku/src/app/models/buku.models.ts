c1f688e637b828fe3d3b58dc090bb60d99eb8076
Wildan728 <shahabwildan1225@gmail.com>
2024-11-19 11:41:28 +0700

    Update

--- prjbuku/src/app/models/buku.models.ts
+++ prjbuku/src/app/models/buku.models.ts
@@ -1,14 +1,5 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
-
-@Component({
-    selector : 'app-buku',
-    templateUrl : './buku.component.html',
-    styleUrl: './buku.component.css'
-})
-export class BukuComponent {
-    simpanBuku(form : NgForm){
-        console.log("Pengujian Klik"),
-        console.log(form.value.judul);
-    }
+export interface Buku{
+    judul : string,
+    penulis : string,
+    genre : string[]
 }
